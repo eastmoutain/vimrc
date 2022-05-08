3986a534bb8be5fad49bcc1986026853b889c53b
thomas <yangwencheng@hygon.cn>
2022-05-08 20:27:46 +0800

    update myprofile

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -3,7 +3,7 @@
 echo "hello"
 
 # install tools
-apt install -y openssh-server curl autoconf automake libncurses5-dev cmake vim git gcc g++ pkg-config  cscope
+sudo apt install -y openssh-server curl autoconf automake libncurses5-dev cmake vim git gcc g++ pkg-config  cscope
 
 # install vundle for vim plugins managment
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -12,18 +12,21 @@ mkdir software -p && cd software
 
 git clone https://github.com/universal-ctags/ctags
 cd ctags
-./autogen && ./configure --prefix="/usr/local" && make -j$(nproc) && make install
+./autogen && ./configure --prefix="/usr/local" && make -j$(nproc) && sudo make install
 
 #install zsh
-apt install zsh && chsh -s $(which zsh)
+sudo apt install zsh && chsh -s $(which zsh)
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#install fzf 
+#install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git
 ./fzf/install
 
 git clone https://github.com/eastmoutain/vimrc
-cd vimrc && cp vimrc ~/.vimrc
+cp vimrc/vimrc ~/.vimrc
+cp vimrc/myprofile ~/.myprofile
+# add this line at the end of ~/.zshrc
+#[ -f ~/.myprofile ] && source ~/.myprofile
 
