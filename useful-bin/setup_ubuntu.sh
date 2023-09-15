c17d0fb3dfb3361f43b84d9c72be22bcdd76ec54
haha <abc.com>
2023-09-15 22:04:20 +0800

    add scripts to setup newly installed ubuntu and centos OS

---
+++ useful-bin/setup_ubuntu.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+
+# for centos: /etc/yum.conf
+# proxy=http://IP:port
+# ip_resolve=IPv4
+
+# for ubuntu
+# create /etc/apt/apt.conf.d/99force-ipv4
+# add "Acquire::ForceIPv4 "true";"
+
+# ~/.bashrc
+# http_proxy="http://IP:port
+# https_proxy="http://IP:port
+
+# sudo use proxy, add the line in /etc/sudoers
+# Defaults    env_keep += "ftp_proxy http_proxy https_proxy no_proxy"
+
+# add /usr/local/lib to library search list
+# add "/usr/local/lib" in /etc/ld.so.conf.d/usr-local.conf
+echo "please setup system proxy, then remove return, and try again"
+
+return
+
+
+sudo apt update
+sudo apt install build-essential -y
+
+sudo apt install -y gcc g++ git curl
+sudo apt install -y git autoconf automake binutils bison flex gettext libtool make patch \
+                    elfutils indent patchutils curl cscope zsh libffi-dev libncurses5-dev  zlib1g-dev
+
+sudo apt install -y strace trace-cmd
+
+mkdir software
+cd software
+git clone https://github.com/eastmoutain/vimrc
+cp vimrc/vimrc /home/thomas/.vimrc
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#install ctags
+sudo rm -rf ctags
+git clone https://github.com/universal-ctags/ctags
+cd ctags
+./autogen.sh && ./configure --prefix=/usr/local && make -j4 && sudo make install
+cd ..
+
+#install openssl
+sudo rm -rf  openssl-1.1.1w
+wget https://www.openssl.org/source/openssl-1.1.1w.tar.gz
+tar zxvf openssl-1.1.1w.tar.gz
+cd openssl-1.1.1w
+./config --prefix=/usr --openssldir=/usr shared zlib
+make
+make test
+sudo make install
+cd ..
+
+#install cmake3
+sudo rm -rf cmake-3.27.5-linux-x86_64.sh
+wget https://github.com/Kitware/CMake/releases/download/v3.27.5/cmake-3.27.5-linux-x86_64.sh
+sudo chmod +x cmake-3.27.5-linux-x86_64.sh
+./cmake-3.27.5-linux-x86_64.sh --skip-license --prefix=/usr/local
+
+# install python3.10
+sudo rm -rf Python-3.10.13.tgz Python-3.10.13
+wget https://www.python.org/ftp/python/3.10.13/Python-3.10.13.tgz
+sudo tar xvf Python-3.10.13.tgz
+cd Python-3.10.13
+./configure --enable-optimizations --enable-shared
+sudo make -j64 altinstall
+cd ..
+
+
+#install vim
+rm -rm -rf vim
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-features=huge --enable-multibyte \
+    --enable-rubyinterp=yes --enable-python3interp=yes \
+    --with-python3-command=python3.10 \
+    --with-python3-config-dir=/usr/local/lib/python3.10/config-3.10-x86_64-linux-gnu \
+    --enable-cscope \
+    --prefix=/usr/local \
+    --enable-fail-if-missing
+make -j64 && sudo make install
+cd ..
+
+##install fzf
+#git clone --depth 1 https://github.com/junegunn/fzf.git fzf
+#cd fzf && ./install
+#cd ..
+
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
